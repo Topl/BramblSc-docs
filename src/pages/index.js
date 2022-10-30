9bfb3a1467b6eee921fcd4612a9fbf80e8093a33
tuxman <n.edmonds@topl.me>
2022-10-30 15:28:40 -0500

    Update homepage with sdk-spec language and links

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="docs/Language-Independent Specification for Implementing the Topl SDK/Introduction to the Language-Independent Topl SDK Specification">
+            SDK Specification
           </Link>
         </div>
       </div>
